0694b737a88ff4db1461bce0de2e7dec1980668d
sanjithsjadhav <sanjithjadhav365@gmail.com>
2020-10-27 17:00:17 +0530

    second test pass

--- src/main/com/sanjith/fizzbuzz/FizzBuzz.java
+++ src/main/com/sanjith/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,10 @@ package com.sanjith.fizzbuzz;
 public class FizzBuzz {
     public String convert(int toConvertToFizzBuzz) {
 
+    	if(toConvertToFizzBuzz%3==0) {
+            return "Fizz";
+        }
+
         return String.valueOf(toConvertToFizzBuzz);
 
     }
